9c0789a68bf540fd55695b24c0feb591bd58106c
Arti Sharma <139053448+ARTI-SHARMA97@users.noreply.github.com>
2023-09-17 19:53:08 +0530

    file add

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Arti123:Pass123@cluster0.io4zqt7.mongodb.net/?retryWrites=true&w=majority');
+
+// Code for using the cloud mongodb atlas 
+const connectParams={
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
+
+// Code for manually using the mongodb of local system
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'error connecting to database'));
+
+db.once('open', ()=>{
+    console.log("successfully connected to database : mongoDB");
+});
+
+module.exports = mongoose;
